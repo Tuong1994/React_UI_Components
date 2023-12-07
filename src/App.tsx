6feb512b56793574101bc37c52aa72b52303d7a8
unknown <nhambontuong68@gmail.com>
2023-12-07 16:25:45 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { UI, Control } from "@/components";
 import "./style/main.scss";
-import { SelectOptions } from "./components/Control/type";
-import { FaUser } from "react-icons/fa";
-import { TabsItems } from "./components/UI/Tabs/type";
 
 const { Section, Button, Tabs } = UI;
 
@@ -19,17 +16,9 @@ const initialData: FormData = {
   role: ["super-admin"],
 };
 
-const items: TabsItems = [
-  { id: "1", title: "Tabs 1", content: "content 1" },
-  { id: "2", title: "Tabs 2", content: "content 2" },
-  { id: "3", title: "Tabs 3", content: "content 3" },
-  { id: "4", title: "Tabs 4", content: "content 4" },
-];
-
 function App() {
   return (
     <Section>
-      <Tabs color="purple" items={items} />
 
       {/* <Form<FormData> initialData={initialData} onFinish={(data) => console.log(data)}>
         <FormItem name="remembered">
