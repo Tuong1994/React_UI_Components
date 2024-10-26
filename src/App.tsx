010daeaf7ac68aa1ce7049d6021c87897b0f4dc0
unknown <nhambontuong68@gmail.com>
2024-10-26 11:00:29 +0700

    fixed Control UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,108 @@
 import { UI, Control } from "@/components";
 import "./style/main.scss";
+import useLayoutStore from "./components/UI/Layout/LayoutStore";
+import { Divider, Image } from "./components/UI";
 
-const { Section, Button } = UI;
+const { Section, Button, Layout } = UI;
 
-const { Form, FormItem, Input, InputPassword, InputPhone, TextArea, Select, SelectTag, TreeSelect } = Control;
+const { Container, Content } = Layout;
+
+const {
+  Form,
+  FormItem,
+  Input,
+  InputPassword,
+  InputPhone,
+  TextArea,
+  DatePicker,
+  Select,
+  SelectTag,
+  Radio,
+  CheckBox,
+} = Control;
 
 interface Data {
   email: string;
   password: string;
   phone: string;
+  message: string;
+  gender: string;
+  birthday: Date | string;
   role: number[];
 }
 
 function App() {
+  const [mode, onSwitchMode] = useLayoutStore((state) => [state.layoutTheme, state.onSwitchTheme]);
+
   const intialValues: Data = {
     email: "",
     password: "",
     phone: "",
+    message: "",
+    gender: "",
+    birthday: new Date(),
     role: [],
   };
 
   const handleFinish = (data: Data) => console.log(data);
 
+  const handleClick = () => {
+    if (mode === "dark") return onSwitchMode("light");
+    onSwitchMode("dark");
+  };
+
   return (
-    <Section>
-      <Form<Data> autoFocusValidation={false} initialData={intialValues} onFinish={handleFinish}>
-        <FormItem name="email" rules={[{ required: true, message: "This field is required" }]}>
-          <Input />
-        </FormItem>
-        <FormItem name="password" rules={[{ required: true, message: "This field is required" }]}>
-          <InputPassword />
-        </FormItem>
-        <FormItem name="phone" rules={[{ required: true, message: "This field is required" }]}>
-          <InputPhone />
-        </FormItem>
-        <FormItem name="role" rules={[{ min: 1, message: "This field is required" }]}>
-          <SelectTag
-            options={[
-              { label: "User", value: 1 },
-              { label: "Admin", value: 2 },
-            ]}
-          />
-        </FormItem>
-        <Button>Submit</Button>
-      </Form>
-    </Section>
+    <Container>
+      <Content>
+        <Section>
+          <Button onClick={handleClick}>Change mode</Button>
+          <Image hasCheck imgWidth={150} imgHeight={150} />
+          <Divider />
+          <Form<Data> autoFocusValidation={false} initialData={intialValues} onFinish={handleFinish}>
+            <FormItem name="email">
+              <Input />
+            </FormItem>
+            <FormItem name="password">
+              <InputPassword />
+            </FormItem>
+            <FormItem name="phone">
+              <InputPhone />
+            </FormItem>
+            <FormItem name="gender">
+              <Select
+                options={[
+                  { label: "Male", value: "male" },
+                  { label: "Female", value: "female" },
+                ]}
+              />
+            </FormItem>
+            <FormItem name="birthday">
+              <DatePicker />
+            </FormItem>
+            <FormItem name="role">
+              <SelectTag
+                options={[
+                  { label: "User", value: 1 },
+                  { label: "Admin", value: 2 },
+                ]}
+              />
+            </FormItem>
+            <FormItem name="message">
+              <TextArea />
+            </FormItem>
+            <FormItem name="gender">
+              <CheckBox value="male" label="Male" />
+              <CheckBox value="female" label="Female" />
+            </FormItem>
+            <FormItem name="gender">
+              <Radio value="male" label="Male" />
+              <Radio value="female" label="Female" />
+            </FormItem>
+            <Button>Submit</Button>
+          </Form>
+        </Section>
+      </Content>
+    </Container>
   );
 }
 
--- src/components/UI/Image/index.tsx
+++ src/components/UI/Image/index.tsx
@@ -42,7 +42,7 @@ const Image: ForwardRefRenderFunction<HTMLImageElement, ImageProps> = (
     imgHeight,
     objectFit = "fill",
     lazyType = "lazy",
-    src = "/default-image.jpg",
+    src = "https://media.istockphoto.com/id/1396814518/vector/image-coming-soon-no-photo-no-thumbnail-image-available-vector-illustration.jpg?s=612x612&w=0&k=20&c=hnh2OZgQGhf0b46-J2z7aHbIWwq8HNlSDaNp2wn_iko=",
     onCheck,
     ...restProps
   },
