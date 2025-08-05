f12dd7dc610462a86b3eea4cd21c2c27db4811af
Tuong1994 <nhambontuong68@gmail.com>
2025-08-05 08:04:09 +0700

    fixed css toast message

--- src/Docs.tsx
+++ src/Docs.tsx
@@ -1,40 +1,37 @@
-import { Alert, Space, Button } from "./components/UI";
+import { ToastMessage, Space, Button } from "./components/UI";
+import useMessage from "./components/UI/ToastMessage/useMessage";
 import {
-  HiCheckCircle as SuccessIcon,
-  HiXCircle as ErrorIcon,
-  HiInformationCircle as InfoIcon,
+  HiOutlineCheckCircle as SuccessIcon,
+  HiOutlineXCircle as ErrorIcon,
+  HiOutlineInformationCircle as InfoIcon,
 } from "react-icons/hi2";
-import { PiWarningCircleFill as WarningIcon } from "react-icons/pi";
-import useAlert from "./components/UI/Alert/useAlert";
+import { PiWarningCircle as WarningIcon } from "react-icons/pi";
 
 const App: React.FC = () => {
-  const alertApi = useAlert({
-    placement: "top",
-    icons: {
-      successIcon: <SuccessIcon />,
-      errorIcon: <ErrorIcon />,
-      infoIcon: <InfoIcon />,
-      warningIcon: <WarningIcon />,
-    },
+  const messageApi = useMessage({
+    successIcon: <SuccessIcon size={20} />,
+    errorIcon: <ErrorIcon size={20} />,
+    infoIcon: <InfoIcon size={20} />,
+    warningIcon: <WarningIcon size={20} />,
   });
 
   return (
     <>
       <Space>
-        <Button color="green" onClick={() => alertApi.success("This is a success message")}>
+        <Button color="green" onClick={() => messageApi.success("This is a success message")}>
           Success
         </Button>
-        <Button color="red" onClick={() => alertApi.error("This is a error message")}>
+        <Button color="red" onClick={() => messageApi.error("This is a error message")}>
           Error
         </Button>
-        <Button color="orange" onClick={() => alertApi.warning("This is a warning message")}>
-          Warningss
+        <Button color="orange" onClick={() => messageApi.warning("This is a warning message")}>
+          Warning
         </Button>
-        <Button color="blue" onClick={() => alertApi.info("This is a info message")}>
+        <Button color="blue" onClick={() => messageApi.info("This is a info message")}>
           Info
         </Button>
       </Space>
-      <Alert />
+      <ToastMessage />
     </>
   );
 };
