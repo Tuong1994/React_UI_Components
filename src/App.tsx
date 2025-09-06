a08ee1fa5697c87ecdeae38ed53ca3f4dccfea91
Tuong1994 <77843790+Tuong1994@users.noreply.github.com>
2025-09-06 14:40:36 +0000

    update css Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import { UI, Control } from "@/components";
 import { ETheme } from "./components/UI/Layout/Context";
 import useLayoutStore from "./components/UI/Layout/LayoutStore";
-import useParticles from "./components/UI/BgAnimation/BgParticles/useParticles";
 import "./style/main.scss";
 
-const { Layout, Carousel } = UI;
+const { Layout, Space, Section, Divider, Button } = UI;
 
 const { Container, Head, Body, Side, Content } = Layout;
 
 const {} = Control;
 
-const { Gallery } = Carousel;
-
 function App() {
   const [mode, onSwitchMode] = useLayoutStore((state) => [state.layoutTheme, state.onSwitchTheme]);
-
-  const { collideOptions } = useParticles(true);
 
   const handleClick = () => {
     if (mode === ETheme.DARK) return onSwitchMode(ETheme.LIGHT);
@@ -24,19 +19,28 @@ function App() {
 
   return (
     <Container style={{ height: "100vh" }}>
-      <Gallery open />
-      {/* <Head></Head>
+      <Head></Head>
       <Body>
         <Side collapsable></Side>
         <Content>
           <Section>
-            <Button color="green" onClick={handleClick}>
+            <Button color="blue" onClick={handleClick}>
               Change mode
             </Button>
             <Divider />
+            <Space>
+              <Button ghost color="blue">Button</Button>
+              <Button ghost color="red">Button</Button>
+              <Button ghost color="green">Button</Button>
+              <Button ghost color="orange">Button</Button>
+              <Button ghost color="yellow">Button</Button>
+              <Button ghost color="purple">Button</Button>
+              <Button ghost color="pink">Button</Button>
+              <Button ghost color="black">Button</Button>
+            </Space>
           </Section>
         </Content>
-      </Body> */}
+      </Body>
     </Container>
   );
 }
